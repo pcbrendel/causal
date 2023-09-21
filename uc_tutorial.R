2de35b1daee0b64c9b7a938bb6059b2188ab2928
Paul Brendel <pcbrendel@gmail.com>
2023-09-21 09:30:38 -0400

    cleaned uc tutorial

--- uc_tutorial.R
+++ uc_tutorial.R
@@ -1,82 +1,174 @@
-# A Regression weighting approach to uncontrolled confounding
-# by Paul Brendel
+# Adjusting for Uncontrolled Confounding
 
-# Create data --------------------------------------------------------------------------------------
+library(tidyverse)
+
+# DERIVE DATA
 
 set.seed(1234)
 n <- 100000
 
-C <- rbinom(n, 1, .5)
-U <- rbinom(n, 1, .5)
-X  <- rbinom(n, 1, plogis(-.5 + .5 * C + 1.5 * U))
-Y  <- rbinom(n, 1, plogis(-.5 + log(2) * X + .5 * C + 1.5 * U))
+c <- rbinom(n, 1, .5)
+u <- rbinom(n, 1, .5)
+x <- rbinom(n, 1, plogis(-.5 + .5 * c + 1.5 * u))
+y <- rbinom(n, 1, plogis(-.5 + log(2) * x + .5 * c + 1.5 * u))
 
-#note that P(Y=1|X=1,C=c,U=u)/P(Y=1|X=0,C=c,U=u) should equal expit(log(2))
-#thus odds(Y=1|X=1,C=c,U=u)/odds(Y=1|X=0,C=c,U=u) = ORyx = exp(log(2)) = 2 
+df <- data.frame(X = x, Y = y, C = c, U = u)
+rm(x, y, c, u)
 
-df <- data.frame(X, Y, C, U)
-rm(C, U, X, Y)
+# INSPECT MODELS
 
-# Compare biased model to bias-free model ------------------------------------------------------------
+nobias_model <- glm(Y ~ X + C + U,
+                    family = binomial(link = "logit"),
+                    data = df)
+exp(summary(nobias_model)$coef[2, 1])
+c(exp(summary(nobias_model)$coef[2, 1] +
+        summary(nobias_model)$coef[2, 2] * qnorm(.025)),
+  exp(summary(nobias_model)$coef[2, 1] +
+        summary(nobias_model)$coef[2, 2] * qnorm(.975)))
+# 2.02 (1.96, 2.09)
 
-no_bias <- glm(Y ~ X + C + U, family = binomial(link = "logit"), data = df) #model with no bias
-exp(coef(no_bias)[2])
-exp(coef(no_bias)[2] + summary(no_bias)$coef[2, 2] * qnorm(.025))
-exp(coef(no_bias)[2] + summary(no_bias)$coef[2, 2] * qnorm(.975))
-#ORyx = 2.02 (1.96, 2.09)
+bias_model <- glm(Y ~ X + C,
+                  family = binomial(link = "logit"),
+                  data = df)
+exp(summary(bias_model)$coef[2, 1])
+c(exp(summary(bias_model)$coef[2, 1] +
+        summary(bias_model)$coef[2, 2] * qnorm(.025)),
+  exp(summary(bias_model)$coef[2, 1] +
+        summary(bias_model)$coef[2, 2] * qnorm(.975)))
+# 3.11 (3.03, 3.20)
 
-uc_bias <- glm(Y ~ X + C, family = binomial(link = "logit"), data = df) #model with uncontrolled confounding
-exp(coef(uc_bias)[2])
-exp(coef(uc_bias)[2] + summary(uc_bias)$coef[2, 2] * qnorm(.025))
-exp(coef(uc_bias)[2] + summary(uc_bias)$coef[2, 2] * qnorm(.975))
-#ORyx = 3.11 (3.03, 3.20)
+# OBTAIN BIAS PARAMETERS
 
-# Model P(U=1|X,C,Y) and obtain regression parameters ------------------------------------------------
+u_model <- glm(U ~ X + C + Y,
+               family = binomial(link = "logit"),
+               data = df)
+summary(u_model)
 
-u_model <- glm(U ~ X + C + Y, family = binomial(link = "logit"), data = df)
-U0 <- coef(u_model)[1]
-U1 <- coef(u_model)[2]
-U2 <- coef(u_model)[3]
-U3 <- coef(u_model)[4]
+# ADJUST
 
-# Create bootstrap function --------------------------------------------------------------------------
+# imputation approach
+adjust_uc_imp_loop <- function(
+  coef_0, coef_x, coef_c, coef_y, nreps, plot = FALSE
+) {
 
-fun <- function (cU, cUX, cUC, cUY) {
-  set.seed(1234)
   est <- vector()
-  nreps <- 10 #can vary number of bootstrap samples
-  
-  for(i in 1:nreps){
-    bdf <- df[sample(1:nrow(df), n, replace=TRUE), ] #random samping with replacement
-    
-    pU <- plogis(cU + cUX * bdf$X + cUC * bdf$C + cUY * bdf$Y) #model the probability of U
-    
-    combined <- bdf[rep(seq_len(nrow(bdf)), 2), ] #duplicate data
-    combined$Usim <- rep(c(1, 0), each=n) #Usim=1 in first copy, Usim=0 in second copy
-    combined$pU <- c(pU, 1 - pU) #when Usim=1, pU is prob of U=1; when Usim=0, pU is prob of U=0
-    
-    Final <- glm(Y ~ X + C + Usim, family = binomial(link = "logit"), weights=pU, data=combined)
-    est[i] <- coef(Final)[2]
+  for (i in 1:nreps){
+    # bootstrap sample
+    bdf <- df[sample(seq_len(n), n, replace = TRUE), ]
+
+    # impute U
+    bdf$Upred <- rbinom(n, 1, plogis(coef_0 + coef_x * bdf$X +
+                                       coef_c * bdf$C + coef_y * bdf$Y))
+
+    final_model <- glm(Y ~ X + C + Upred, family = binomial(link = "logit"),
+                       data = bdf)
+    est[i] <- exp(coef(final_model)[2])
   }
-  
-  out <- list(exp(median(est)), exp(quantile(est, c(.025, .975))), hist(exp(est)))
+
+  out <- list(
+    estimate = round(median(est), 2),
+    ci = round(quantile(est, c(.025, .975)), 2)
+  )
+
+  if (plot) {
+    out$hist <- hist(exp(est))
+  }
+
   return(out)
+
 }
 
-# Apply functions ------------------------------------------------------------------------------------
+# using known, correct bias parameters
+set.seed(1234)
+correct_results <- adjust_uc_imp_loop(
+  coef_0 = coef(u_model)[1],
+  coef_x = coef(u_model)[2],
+  coef_c = coef(u_model)[3],
+  coef_y = coef(u_model)[4],
+  nreps = 10
+)
 
-# Known, correct bias parameters   
-fun(cU = U0, cUX = U1, cUC = U2, cUY = U3)
-# ORyx = 2.02 (1.98, 2.06)
+correct_results$estimate
+correct_results$ci
+# 2.03 (1.95, 2.10)
 
-# Incorret bias parameters
-fun(cU = 2 * U0, cUX = 2 * U1, cUC = 2 * U2, cUY = 2 * U3)
-# ORyx = 0.80 (0.79, 0.82)
+# using incorret bias parameters
+set.seed(1234)
+incorrect_results <- adjust_uc_imp_loop(
+  coef_0 = coef(u_model)[1] * 2,
+  coef_x = coef(u_model)[2] * 2,
+  coef_c = coef(u_model)[3] * 2,
+  coef_y = coef(u_model)[4] * 2,
+  nreps = 10
+)
 
+incorrect_results$estimate
+incorrect_results$ci
+# 0.81 (0.78, 0.84)
 
+# weighting approach
+adjust_uc_wgt_loop <- function(
+  coef_0, coef_x, coef_c, coef_y, nreps, plot = FALSE
+) {
 
+  est <- vector()
+  for (i in 1:nreps){
+    # bootstrap sample
+    bdf <- df[sample(seq_len(n), n, replace = TRUE), ]
 
+    # the probability of U for each observation
+    prob_u <- plogis(coef_0 + coef_x * bdf$X + coef_c * bdf$C + coef_y * bdf$Y)
 
+    # duplicate data
+    combined <- dplyr::bind_rows(bdf, bdf)
+    # assign values for U
+    combined$Ubar <- rep(c(1, 0), each = n)
+    # create weight
+    # when Ubar=1, u_weight=P(U=1); when Ubar=0, u_weight=P(U=0)
+    combined$u_weight <- c(prob_u, 1 - prob_u)
 
+    final_model <- glm(Y ~ X + C + Ubar, family = binomial(link = "logit"),
+                       data = combined, weights = combined$u_weight)
+    est[i] <- exp(coef(final_model)[2])
+  }
 
+  out <- list(
+    estimate = round(median(est), 2),
+    ci = round(quantile(est, c(.025, .975)), 2)
+  )
 
+  if (plot) {
+    out$hist <- hist(exp(est))
+  }
+
+  return(out)
+
+}
+
+# using known, correct bias parameters
+set.seed(1234)
+correct_results <- adjust_uc_wgt_loop(
+  coef_0 = coef(u_model)[1],
+  coef_x = coef(u_model)[2],
+  coef_c = coef(u_model)[3],
+  coef_y = coef(u_model)[4],
+  nreps = 10
+)
+
+correct_results$estimate
+correct_results$ci
+# 2.03 (1.98, 2.07)
+
+# using incorret bias parameters
+set.seed(1234)
+incorrect_results <- adjust_uc_wgt_loop(
+  coef_0 = coef(u_model)[1] * 2,
+  coef_x = coef(u_model)[2] * 2,
+  coef_c = coef(u_model)[3] * 2,
+  coef_y = coef(u_model)[4] * 2,
+  nreps = 10
+)
+
+incorrect_results$estimate
+incorrect_results$ci
+# 0.81 (0.79, 0.82)
